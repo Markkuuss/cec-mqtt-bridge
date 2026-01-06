1e2e4d2100080e5b24427347be540551ace59b2a
Markus Schraufstetter <10926460+Markkuuss@users.noreply.github.com>
2026-01-06 23:31:09 +0100

    Use venv for Debian/Ubuntu install

--- contrib/debian-ubuntu-install.sh
+++ contrib/debian-ubuntu-install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 PREFIX="/usr/local"
+VENV_DIR="/opt/cec-mqtt-bridge/venv"
 
 cd ..
 
 python3 -m build
-sudo pip install --force-reinstall dist/cec_mqtt_bridge-*.whl
+
+sudo mkdir -p /opt/cec-mqtt-bridge
+sudo python3 -m venv "$VENV_DIR"
+sudo "$VENV_DIR/bin/python" -m pip install --upgrade pip
+sudo "$VENV_DIR/bin/pip" install --force-reinstall dist/cec_mqtt_bridge-*.whl
 
 if [ -f /etc/cec-mqtt-bridge.ini ]; then
   echo "/etc/cec-mqtt-bridge.ini exists, did not copy new config, you may need to edit existing!" 
@@ -18,3 +23,5 @@ sudo install -m 644 -C debian/cec-mqtt-bridge.service /etc/systemd/system/cec-mq
 sudo systemctl enable cec-mqtt-bridge
 sudo systemctl daemon-reload
 sudo systemctl start cec-mqtt-bridge
+
+
